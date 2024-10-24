e2be6644c7f493a090c51c3b9f9d8a60534511b4
Es <fatih@itsmoneo.com>
2024-10-24 12:32:56 +0300

    added project docker part for FE

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+        <div className={'flex w-full h-dvh flex-row justify-center items-center'}>
+            <h1 className={'text-3xl'}>Hello Panteon, this is Fatih!</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
